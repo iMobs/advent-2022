b5c4834d74a224a7cb4aafdf8e7f55e3bf16b0ba
Ian Mobley <ianmobley@gmail.com>
2022-12-06 10:34:15 -0800

    feat: day06
    This one was pretty easy and even refactoring to shared code was simple.

---
+++ day06/src/lib.rs
@@ -0,0 +1,51 @@
+use std::collections::{HashSet, VecDeque};
+
+pub fn challenge_1(input: &str) -> usize {
+    find_unique_limit(input, 4)
+}
+
+pub fn challenge_2(input: &str) -> usize {
+    find_unique_limit(input, 14)
+}
+
+fn find_unique_limit(input: &str, limit: usize) -> usize {
+    let mut queue = VecDeque::new();
+
+    for (i, c) in input.chars().enumerate() {
+        queue.push_back(c);
+
+        if queue.len() == limit {
+            let set: HashSet<_> = queue.iter().collect();
+            if set.len() == limit {
+                return i + 1;
+            }
+
+            queue.pop_front();
+        }
+    }
+
+    unreachable!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_challenge_1() {
+        assert_eq!(challenge_1("mjqjpqmgbljsphdztnvjfqwrcgsmlb"), 7);
+        assert_eq!(challenge_1("bvwbjplbgvbhsrlpgdmjqwftvncz"), 5);
+        assert_eq!(challenge_1("nppdvjthqldpwncqszvftbrmjlhg"), 6);
+        assert_eq!(challenge_1("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"), 10);
+        assert_eq!(challenge_1("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"), 11);
+    }
+
+    #[test]
+    fn test_challenge_2() {
+        assert_eq!(challenge_2("mjqjpqmgbljsphdztnvjfqwrcgsmlb"), 19);
+        assert_eq!(challenge_2("bvwbjplbgvbhsrlpgdmjqwftvncz"), 23);
+        assert_eq!(challenge_2("nppdvjthqldpwncqszvftbrmjlhg"), 23);
+        assert_eq!(challenge_2("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"), 29);
+        assert_eq!(challenge_2("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"), 26);
+    }
+}
